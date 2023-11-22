8d68f02c022b7c891fbe8e5905fc0c359047149b
Jake Meetre <jakemeetre@gmail.com>
2023-11-21 22:14:56 -0500

    Added js background from vanta.js fixed h1 and nav bar colors, testing commit and on phone

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    VANTA.WAVES({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x171818,
+        shininess: 43.00,
+        waveHeight: 11.00,
+        waveSpeed: 0.75,
+        zoom: 1.04
+    });
+});
